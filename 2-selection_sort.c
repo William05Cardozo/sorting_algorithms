87b51ed7bd7eaf365d65ca065acd23ba441e9829
William05Cardozo <3784@holbertonschool.com>
2022-02-16 00:37:15 -0800

    Add-Comments-Correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,21 @@
 #include "sort.h"
 
+/**
+ * selection_sort - Function of selection and sort integers
+ * @array: Array to print and sort (pointer)
+ * @size: Nimber of elements in array
+ * a: Iterator
+ * b: Iterator
+ * c: variable storing data
+ * Return: No Line
+ **/
+
 void selection_sort(int *array, size_t size)
 {
 	int selec;
 	size_t a, b, c;
 
-	for (a = 0; a <size; a++)
+	for (a = 0; a < size; a++)
 	{
 		c = a;
 		for (b = a + 1; b < size; b++)
