6e3c33b11b2a072491d8e0c90d566a50b3c2f15b
William05Cardozo <3784@holbertonschool.com>
2022-02-15 22:56:36 -0800

    Bubble-Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t a, b;
+	int bubb;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = 0; b < size - a - 1; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				bubb = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = bubb;
+				print_array(array, size);
+			}
+
+		}
+	}
+
+}
